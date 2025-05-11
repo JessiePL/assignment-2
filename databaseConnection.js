387a71d479c3d52655187f59b74c261d7108c012
JessiePL <240835494@qq.com>
2025-05-10 22:08:46 -0700

    test mongodb uri

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,6 +5,7 @@ const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_database = process.env.MONGODB_DATABASE;
+console.log("MONGODB_HOST = ", process.env.MONGODB_HOST);
 
 const atlasURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/?retryWrites=true&w=majority`;
 
